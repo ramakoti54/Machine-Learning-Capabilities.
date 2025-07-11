b7c2deee38d5946564bd05388fbe8b7f40f77be8
Ram T <44626534+ramakoti54@users.noreply.github.com>
2025-07-11 13:16:11 +0530

    Web_Scrapping.py
    Web Scrapping methodologies to support Natural Language Processing for N Gram model of LLM. The idea is to identify the positive comments on the business growth of a stock. It could recommend the customer whether the Stock ticker for Financial institutions to be a potential Buy option. The discussion on the ticker would be recommended once the Scrapping content is send as an input to the model along with our dictionary of significant information related to changes in the industry or Organizational growth factors  impacting suggested outcome to be one of the two possible classification labels(Buy,Sell).

---
+++ Web_Scrapping.py
@@ -0,0 +1,78 @@
+# Defining Modules used for Designing N-Gram model of LLM.
+import tensorflow as tf;
+import os;
+from tensorflow import keras;
+#import URLlib.request as urs;
+import webbrowser as wb;
+import re;
+import urllib.request as urs
+
+# Algorithmic program to illustrate Machine Learning algorithms usage on Webscrapping from X
+# Step1: Forming a vector/array with the required token sizes for storing different words of an Web URL.
+# Step2: Calling URLlib.request module to connect to the respective website X with an URL as "https://x.com/"
+# Step3:
+
+
+# 1. Time+Space complexity to use Vector vs Dictionary to understand usage of datatypes.
+# 2. Using the same string for tf.tensor to represent data types.
+
+class N_Model:
+   def Web_scrapping(self, URL,Content):
+      with urs.urlopen(URL):
+         response = urs.response(URL);
+         Content = response.read();
+         return (str(Content));
+
+   def Tokenization(Content):
+      #for con in Content:
+      #   if(re.split('.',Content,re.IGNORECASE)):
+      X_Content = re.split('\. ',Content,re.IGNORECASE);
+      k = len(X_Content);
+      vector_tokens = [[0,0,0],[0,0,0],[0,0,0]];
+      Sentences = "";
+      Words = [];
+      for tokens in  X_Content:
+         #for tokens in X_Content:
+         #token[tokens] = i;
+         #i = i+1;
+         #print(vector_tokens);
+         #Sentences.append('tokens');
+         Sentences = tokens;
+         Split_Sentence = Sentences.split(" ");
+         Words.append(Split_Sentence);
+      return(Words);
+
+   # Move NGram_Model outside of Tokenization
+   def NGram_Model(Tokens,i,j,Token,Token_Values):
+      for words in Tokens:
+         for word in words:
+            #print(word)
+            if(i < len(words)):
+               Token.append(word);
+               Token_Values.append(i);
+               #for i in word:
+               #   print(word[i]);
+               i = i+1;
+            else:
+               #print("Number of words are zero. Check the Webpage comments");
+               continue;
+      Token_Vector = [Token,Token_Values]
+      return(Token_Vector);
+
+URL = "https://finance.yahoo.com/news/july-rate-cut-from-fed-is-now-completely-off-the-table-following-solid-jobs-report-141432543.html";
+#Token_Value = 1;
+#Vector = [];
+Content = "";
+#Tokens = [];
+#Vector_Size =
+
+N_model_object = N_Model();
+X_Content = N_model_object.Web_scrapping(URL,Content);
+Tokens = Tokenization(X_Content);
+i=0;
+j=0;
+Token_keys = [];
+Token_Values = [];
+#Token_Vector = N_model_object.NGram_Model(Tokens,i,j,Token_keys,Token_Values);
+#print(Token_Vector[0],"\n",Token_Vector[1]);
+print(Tokens)
